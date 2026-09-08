afdcdf46ea35c7c87f5ceb3ff0fb2b5906dede11
DJTheBest <djthebest53@gmail.com>
2026-09-07 20:06:09 -0500

    Add MinIO API endpoint configuration

---
+++ minio-config.js
@@ -0,0 +1,6 @@
+// Public AitherFiles MinIO API endpoint.
+// Set this to the HTTPS URL of your AitherFiles MinIO API service.
+const minio = {
+  apiUrl: "YOUR_MINIO_API_URL"
+};
+export default minio;
