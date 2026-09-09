0009b52380695c2f368c73fb28404bb83182b588
DJTheBest <djthebest53@gmail.com>
2026-09-09 17:05:41 -0500

    Use deployed AitherFiles Render API URL

--- storage-config.js
+++ storage-config.js
@@ -1,6 +1,6 @@
 // AitherFiles S3-compatible storage API configuration.
 // The browser talks only to the AitherFiles backend; never put S3 access keys here.
 const storageConfig = {
-  apiBase: 'https://aitherfiles.onrender.com'
+  apiBase: 'https://aitherfiles-l5b9.onrender.com'
 };
 export default storageConfig;
